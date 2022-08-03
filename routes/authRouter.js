407d72d1823631cffab795075b7c65a28bfc6496
beatriz.nnds <beatriz.nnds@gmail.com>
2022-08-03 19:36:54 -0300

    feat: delete url from user

---
+++ routes/authRouter.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { validateNewUser } from '../middlewares/validateNewUser.js';
+import { validateSignIn } from '../middlewares/validateSignIn.js';
+import { signUp, signIn } from '../controllers/authController.js';
+
+const router = Router();
+
+router.post('/signup', validateNewUser, signUp);
+router.post('/signin', validateSignIn, signIn)
+
+export default router;
