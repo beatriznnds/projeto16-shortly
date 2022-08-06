0b1b4ba659fcb71fb4ba3231dcb7de828b278e8c
beatriz.nnds <beatriz.nnds@gmail.com>
2022-08-06 17:04:22 -0300

    fix: ranking query

--- controllers/rankingController.js
+++ controllers/rankingController.js
@@ -2,7 +2,7 @@ import connection from "../database.js"
 
 export async function getViews (req, res) {
     try {
-        const { rows: body } = await connection.query(`SELECT urls."userId" AS id, users.name AS name, COUNT(urls.views) AS "linkCount",
+        const { rows: body } = await connection.query(`SELECT users.id AS id, users.name AS name, COUNT(urls.views) AS "linkCount",
         COALESCE(SUM(urls.views),0) AS "visitCount"
         FROM users
         LEFT JOIN urls ON users.id = urls."userId"
