f2bdf4038fee812c55f6c12f7acadf4488b5f09b
beatriz.nnds <beatriz.nnds@gmail.com>
2022-08-06 16:57:46 -0300

    fix: ranking route

--- controllers/rankingController.js
+++ controllers/rankingController.js
@@ -4,9 +4,9 @@ export async function getViews (req, res) {
     try {
         const { rows: body } = await connection.query(`SELECT urls."userId" AS id, users.name AS name, COUNT(urls.views) AS "linkCount",
         COALESCE(SUM(urls.views),0) AS "visitCount"
-        FROM urls
-        LEFT JOIN users ON users.id = urls."userId"
-        GROUP BY users.name, urls."userId"
+        FROM users
+        LEFT JOIN urls ON users.id = urls."userId"
+        GROUP BY users.name, users."userId"
         ORDER BY "visitCount" DESC
         LIMIT 10`);
         return res.send(body).status(200);
