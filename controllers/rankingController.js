0905c9a69770cbd53ce87199f1bd570c3a22e2ad
beatriz.nnds <beatriz.nnds@gmail.com>
2022-08-06 16:46:16 -0300

    fix: get urls from user

--- controllers/rankingController.js
+++ controllers/rankingController.js
@@ -2,9 +2,9 @@ import connection from "../database.js"
 
 export async function getViews (req, res) {
     try {
-        const { rows: body } = await connection.query(`SELECT urls."userId" AS id, users.name AS name, COUNT(urls.views) AS "linkCount", SUM(urls.views) AS "visitCount"
+        const { rows: body } = await connection.query(`SELECT urls."userId" AS id, users.name AS name, COUNT(urls.views) AS "linkCount", COALESCE(SUM((urls.views), 0)) AS "visitCount"
         FROM urls
-        JOIN users ON users.id = urls."userId"
+        LEFT JOIN users ON users.id = urls."userId"
         GROUP BY users.name, urls."userId"
         ORDER BY "visitCount" DESC
         LIMIT 10`);
